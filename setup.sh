01cf74659f14f8293a1f35d11450526b3e7d4b3b
vickydasta <vickydasta@gmail.com>
2016-10-12 23:41:08 +0700

    change passowrd to passwd for no reason

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 exports AHDH_DB_HOST="localhost"
 exports AHDH_DB_PORT="3306"
 exports AHDH_DB_USER="root"
-exports AHDH_DB_PASSWORD="password"
+exports AHDH_DB_PASSWORD="passwd"
 exports AHDH_DB_NAME="ahdh_db"
