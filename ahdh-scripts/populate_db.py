c87595e05fa9105678f691a490f078411584bff2
vickydasta <vickydasta@gmail.com>
2016-10-12 22:55:29 +0700

    pep8 ftw

--- ahdh-scripts/populate_db.py
+++ ahdh-scripts/populate_db.py
@@ -3,22 +3,19 @@
 import MySQLdb as mysql
 import os
 
-AHDH_DB_HOST = os.getenv('AHDH_DB_HOST','localhost')
-AHDH_DB_PORT = os.getenv('AHDH_DB_PORT',3306)
-AHDH_DB_USER = os.getenv('AHDH_DB_USER','root')
-AHDH_DB_PASSWORD = os.getenv('AHDH_DB_PASSWORD','anakamak')
-AHDH_DB_NAME = os.getenv('AHDH_DB_NAME','ahdh_db')
+AHDH_DB_HOST = os.getenv('AHDH_DB_HOST', 'localhost')
+AHDH_DB_PORT = os.getenv('AHDH_DB_PORT', 3306)
+AHDH_DB_USER = os.getenv('AHDH_DB_USER', 'root')
+AHDH_DB_PASSWORD = os.getenv('AHDH_DB_PASSWORD', 'anakamak')
+AHDH_DB_NAME = os.getenv('AHDH_DB_NAME', 'ahdh_db')
 
 db = mysql.connect(host=AHDH_DB_HOST,
                    port=AHDH_DB_PORT,
                    user=AHDH_DB_USER,
                    passwd=AHDH_DB_PASSWORD,
-                   db=AHDH_DB_NAME
-)
+                   db=AHDH_DB_NAME)
 
-names = [
-        "ujang", "uda", "uni", "abi", "gori"
-]
+names = ["ujang", "uda", "uni", "abi", "gori"]
 
 query = """
     INSERT INTO `ahdh` (`id`, `username`) VALUES ("{}", "{}");
@@ -26,13 +23,22 @@ query = """
 
 cur = db.cursor()
 
+# remove existing ahdh table
+cur.execute("DROP TABLE IF EXISTS ahdh;")
+
 # create ahdh table
-cur.execute("CREATE TABLES ahdh(id int NOT NULL, username varchar(20) NOT NULL);")
+cur.execute('''
+            CREATE TABLES ahdh(id int NOT NULL, username varchar(20) NOT NULL);
+            ''')
+
+# commit changes
 db.commit()
 
 # populate table with dummy data
-for i in range(len(names)):
-    sql = query.format(i, names[i])
+for id in range(len(names)):
+    sql = query.format(id, names[i])
     cur.execute(sql)
     db.commit()
+
+# close connection to database
 db.close()
