e2ef0518cb2991c59df543fc516a823550498685
vickydasta <vickydasta@gmail.com>
2016-10-12 22:56:05 +0700

    pep8 ftw

--- ahdh-scripts/generate_urls.py
+++ ahdh-scripts/generate_urls.py
@@ -3,15 +3,14 @@
 import MySQLdb as mysql
 import os
 
-AHDH_DB_HOST = os.getenv('AHDH_DB_HOST','localhost')
-AHDH_DB_PORT = os.getenv('AHDH_DB_PORT',3306)
-AHDH_DB_USER = os.getenv('AHDH_DB_USER','root')
-AHDH_DB_PASSWORD = os.getenv('AHDH_DB_PASSWORD','anakamak')
-AHDH_DB_NAME = os.getenv('AHDH_DB_NAME','ahdh_db')
+AHDH_DB_HOST = os.getenv('AHDH_DB_HOST', 'localhost')
+AHDH_DB_PORT = os.getenv('AHDH_DB_PORT', 3306)
+AHDH_DB_USER = os.getenv('AHDH_DB_USER', 'root')
+AHDH_DB_PASSWORD = os.getenv('AHDH_DB_PASSWORD', 'anakamak')
+AHDH_DB_NAME = os.getenv('AHDH_DB_NAME', 'ahdh_db')
 
 db = mysql.connect(host=AHDH_DB_HOST,
                    port=AHDH_DB_PORT,
                    user=AHDH_DB_USER,
                    passwd=AHDH_DB_PASSWORD,
-                   db=AHDH_DB_NAME
-)
+                   db=AHDH_DB_NAME)
