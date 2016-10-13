23ee2597f19aa1bab79c8d7fa6a89795dc01f1b0
vickydasta <vickydasta@gmail.com>
2016-10-13 09:20:55 +0700

    forgot to comment

--- ahdh-scripts/populate_db.py
+++ ahdh-scripts/populate_db.py
@@ -3,25 +3,30 @@
 import MySQLdb as mysql
 import os
 
+# get environment variables, if none use default value instead
 AHDH_DB_HOST = os.getenv('AHDH_DB_HOST', 'localhost')
 AHDH_DB_PORT = os.getenv('AHDH_DB_PORT', 3306)
 AHDH_DB_USER = os.getenv('AHDH_DB_USER', 'root')
 AHDH_DB_PASSWORD = os.getenv('AHDH_DB_PASSWORD', 'anakamak')
 AHDH_DB_NAME = os.getenv('AHDH_DB_NAME', 'ahdh_db')
 
-db = mysql.connect(host=AHDH_DB_HOST,
-                   port=AHDH_DB_PORT,
-                   user=AHDH_DB_USER,
-                   passwd=AHDH_DB_PASSWORD,
-                   db=AHDH_DB_NAME)
+# connect to database
+db = mysql.connect(host=AHDH_DB_HOST
+                    port=AHDH_DB_PORT
+                    user=AHDH_DB_USER,
+                    passwd=AHDH_DB_PASSWORD,
+                    db=AHDH_DB_NAME)
+cur = db.cursor()
 
-names = ["ujang", "uda", "uni", "abi", "gori"]
+# names in tupple rather than list (immutable ftw)
+names = ("ujang", "uda", "uni", "abi", "gori")
 
+# default query
 query = """
     INSERT INTO `ahdh` (`id`, `username`) VALUES ("{}", "{}");
     """
 
-cur = db.cursor()
+
 
 # remove existing ahdh table
 cur.execute("DROP TABLE IF EXISTS ahdh;")
@@ -36,8 +41,14 @@ db.commit()
 
 # populate table with dummy data
 for id in range(len(names)):
+
+    # use id in range 1-4
     sql = query.format(id, names[i])
+
+    # execute query
     cur.execute(sql)
+
+    # commit changes
     db.commit()
 
 # close connection to database
